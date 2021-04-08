e51675c8ecabaef1c17bcad461c8cebe573dd34a
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 16:46:58 +0200

    Update 4K.js

--- commands/nsfw/4K.js
+++ commands/nsfw/4K.js
@@ -5,11 +5,15 @@ module.exports = {
  name: "fourk",
  description: "drop fourk pictures",
  run: async (client, message, args) => {
-	const image = await nsfw.fourk();
-	const embed = new Discord.MessageEmbed()
-   		 .setTitle(`Fourk Image`)
-    		.setColor("GREEN")
-    		.setImage(image);
-			message.channel.send(embed);
+	 if (this.nsfw = Boolean(true.nsfw)) {
+            
+		const image = await nsfw.fourk();
+		const embed = new Discord.MessageEmbed()
+			 .setTitle(`Fourk Image`)
+			.setColor("GREEN")
+			.setImage(image);
+				message.channel.send(embed);
+	 }
   }
 }
+
