e5ebcde89e4918d505a63cf602b993fb6534cf6f
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 17:09:12 +0200

    Update Neko Feet.js

--- commands/nsfw/Neko Feet.js
+++ commands/nsfw/Neko Feet.js
@@ -5,11 +5,15 @@ module.exports = {
  name: "nekofeet",
  description: "drop Neko feet pictures",
  run: async (client, message, args) => {
+   if (message.channel.nsfw) {
 	const image = await nsfw.nekofeet();
 	const embed = new Discord.MessageEmbed()
    		 .setTitle(`Neko feet Image`)
     		.setColor("GREEN")
     		.setImage(image);
 			message.channel.send(embed);
+   } else {
+	   message.channel.send("only in NSFW channel");
+   }
   }
 }
