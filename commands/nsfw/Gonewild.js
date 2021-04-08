a93ffbd99e4c8c697911cdd8c9b8406a52e63d64
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 17:01:34 +0200

    Update Gonewild.js

--- commands/nsfw/Gonewild.js
+++ commands/nsfw/Gonewild.js
@@ -5,11 +5,15 @@ module.exports = {
  name: "gonewild",
  description: "drop gonewild pictures",
  run: async (client, message, args) => {
+     if (message.channel.nsfw) {
 	const image = await nsfw.gonewild();
 	const embed = new Discord.MessageEmbed()
    		 .setTitle(`Gonewild Image`)
     		.setColor("GREEN")
     		.setImage(image);
 			message.channel.send(embed);
+     } else {
+	     message.channel.send("only in NSFW channel");
+     }
   }
 }
