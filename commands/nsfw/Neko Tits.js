b69165dfe6bc77234093a728dec5fdb077127afe
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 17:11:28 +0200

    Update Neko Tits.js

--- commands/nsfw/Neko Tits.js
+++ commands/nsfw/Neko Tits.js
@@ -5,11 +5,15 @@ module.exports = {
  name: "nekotits",
  description: "drop Neko Tits pictures",
  run: async (client, message, args) => {
+    if (message.channel.nsfw) {
 	const image = await nsfw.nekotits();
 	const embed = new Discord.MessageEmbed()
    		 .setTitle(`Neko tits Image`)
     		.setColor("GREEN")
     		.setImage(image);
 			message.channel.send(embed);
+    } else { 
+	    message.channel.send("only in NSFW channel");
+    }
   }
 }
