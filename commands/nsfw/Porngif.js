c22bdb9143e0fa1d5daf89364737d2903207a388
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 17:12:20 +0200

    Update Porngif.js

--- commands/nsfw/Porngif.js
+++ commands/nsfw/Porngif.js
@@ -5,11 +5,15 @@ module.exports = {
  name: "pgif",
  description: "drop Porn git ",
  run: async (client, message, args) => {
+   if (message.channel.nsfw) {
 	const image = await nsfw.pgif();
 	const embed = new Discord.MessageEmbed()
    		 .setTitle(`Porno gif`)
     		.setColor("GREEN")
     		.setImage(image);
 			message.channel.send(embed);
+   } else { 
+	   message.channel.send("only in NSFW channel");
+   }
   }
 }
