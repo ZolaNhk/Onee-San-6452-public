0e765e1e151fef4d1ada9b0753751fa0ef88df89
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 16:54:00 +0200

    Update Anal.js

--- commands/nsfw/Anal.js
+++ commands/nsfw/Anal.js
@@ -5,11 +5,16 @@ module.exports = {
  name: "anal",
  description: "drop anal pictures",
  run: async (client, message, args) => {
-	const image = await nsfw.anal();
-	const embed = new Discord.MessageEmbed()
-   		 .setTitle(`Anal Image`)
-    		.setColor("GREEN")
-    		.setImage(image);
-			message.channel.send(embed);
+	 if (message.channel.nsfw) {
+            const image = await nsfw.anal();
+		const embed = new Discord.MessageEmbed()
+			 .setTitle(`Anal Image`)
+			.setColor("GREEN")
+			.setImage(image);
+				message.channel.send(embed);
+        } else {
+            message.channel.send("only in NSFW channel");
+        }
+	
   }
 }
