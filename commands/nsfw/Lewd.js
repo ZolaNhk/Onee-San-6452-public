3005fa6b8aa1c33e337d4293b47ef15fd1242efb
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 17:07:56 +0200

    Update Lewd.js

--- commands/nsfw/Lewd.js
+++ commands/nsfw/Lewd.js
@@ -5,11 +5,15 @@ module.exports = {
  name: "lewd",
  description: "drop Lewd pictures",
  run: async (client, message, args) => {
+    if (message.channel.nsfw) {
 	const image = await nsfw.lewd();
 	const embed = new Discord.MessageEmbed()
    		 .setTitle(`Lewd Image`)
     		.setColor("GREEN")
     		.setImage(image);
 			message.channel.send(embed);
+    } else {
+	    message.channel.send("only in NSFW channel");
+    }
   }
 }
