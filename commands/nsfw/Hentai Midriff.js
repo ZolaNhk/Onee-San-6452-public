b5f5e5706220a097e4adb416bb8670748cc79631
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 17:03:35 +0200

    Update Hentai Midriff.js

--- commands/nsfw/Hentai Midriff.js
+++ commands/nsfw/Hentai Midriff.js
@@ -5,11 +5,15 @@ module.exports = {
  name: "hmidriff",
  description: "drop hentai midriff pictures",
  run: async (client, message, args) => {
+    if (message.channel.nsfw) {
 	const image = await nsfw.hmidriff();
 	const embed = new Discord.MessageEmbed()
    		 .setTitle(`Hentai midriff Image`)
     		.setColor("GREEN")
     		.setImage(image);
 			message.channel.send(embed);
+    } else { 
+	    message.channel.send("only in NSFW channel");
+    }
   }
 }
