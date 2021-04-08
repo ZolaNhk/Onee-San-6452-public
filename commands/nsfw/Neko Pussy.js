74a417a37a45d6e73fc9bf3aa40d424aeb9a4915
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 17:10:07 +0200

    Update Neko Pussy.js

--- commands/nsfw/Neko Pussy.js
+++ commands/nsfw/Neko Pussy.js
@@ -5,11 +5,15 @@ module.exports = {
  name: "nekopussy",
  description: "drop Neko pussy pictures",
  run: async (client, message, args) => {
+    if (message.channel.nsfw) {
 	const image = await nsfw.nekopussy();
 	const embed = new Discord.MessageEmbed()
    		 .setTitle(`Neko pussy Image`)
     		.setColor("GREEN")
     		.setImage(image);
 			message.channel.send(embed);
+    } else  {
+	   message.channel.send("only in NSFW channel");
+    }
   }
 }
