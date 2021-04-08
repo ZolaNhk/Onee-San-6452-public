6e81f0a7e1168c5eb126715c7e32e757e470714b
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 16:59:29 +0200

    Update Erokemo.js

--- commands/nsfw/Erokemo.js
+++ commands/nsfw/Erokemo.js
@@ -5,11 +5,15 @@ module.exports = {
  name: "erokemo",
  description: "drop erokemo pictures",
  run: async (client, message, args) => {
+      if (message.channel.nsfw) {
 	const image = await nsfw.erokemo();
 	const embed = new Discord.MessageEmbed()
    		 .setTitle(`erokemo Image`)
     		.setColor("GREEN")
     		.setImage(image);
 			message.channel.send(embed);
+      } else {
+	      message.channel.send("only in NSFW channel");
+      }
   }
 }
