097a297c25e40c17ab80fde4744e106b3a5cb2e1
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 19:56:02 +0200

    Update image.js

--- commands/help/image.js
+++ commands/help/image.js
@@ -3,20 +3,17 @@ const Discord = require("discord.js");
 module.exports = {
 	name: "image",
 	description: "see help image command list",
-	usage: "image"
+	usage: "image",
 	run: async (client, message, args) => {
 
-
-
-
- const embed = new Discord.MessageEmbed()
-   .setTitle("all command for image")
-   .setField("//neko" , 
-     "drop neko SFW image "
-	    )
-   .setTimestamp()
-   .setFooter("discord.gg/PGww3kE4eN")
-     message.channel.send(embed)
+		 const embed = new Discord.MessageEmbed()
+		   .setTitle("all command for image")
+		   .setField("//neko" , 
+		     "drop neko SFW image "
+			    )
+		   .setTimestamp()
+		   .setFooter("discord.gg/PGww3kE4eN")
+		     message.channel.send(embed)
   }
 }
 
