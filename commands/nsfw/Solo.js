3b0601d8d78d41547d5e15ec95dc4058a8e56c64
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 17:14:20 +0200

    Update Solo.js

--- commands/nsfw/Solo.js
+++ commands/nsfw/Solo.js
@@ -5,11 +5,15 @@ module.exports = {
  name: "solo",
  description: "drop Solo porn pictures",
  run: async (client, message, args) => {
+    if (message.channel.nsfw) {
 	const image = await nsfw.solo();
 	const embed = new Discord.MessageEmbed()
    		 .setTitle(`Solo Image`)
     		.setColor("GREEN")
     		.setImage(image);
 			message.channel.send(embed);
+    } else {
+	    message.channel.send("only in NSFW channel");
+    }
   }
 }
