04249f018ea48ba105a783eb8153d46701e31a9e
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 18:49:22 +0200

    Add files via upload

---
+++ commands/play has/status.js
@@ -0,0 +1,29 @@
+const db = require("quick.db");
+const discord = require("discord.js");
+const OnwerId = "593763665459675167"
+
+module.exports = {
+  name: "status",
+  description: "Change the bot status",
+  usage: "status <here>",
+  category: "owner",
+  ownerOnly: true,
+      run: async (client, message, args) => {
+      	if (message.author.id === OwnerId) {
+
+
+			    //ARGUMENT
+			    if(!args.length) {
+			      return message.channel.send("Please give status message")
+			    } 
+			  
+
+			    db.set(`status`, args.join(" "))
+			    client.user.setActivity(args.join(" ")); 
+			    message.channel.send("Updated the bot status");
+			    } else {
+			    	  message.channel.send("only the owner of this bot can do it");
+			    }
+			 }		   
+		 };
+
