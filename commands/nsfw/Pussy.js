9f524ced71143f13c484637e0c337c7551ffe182
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 17:13:20 +0200

    Update Pussy.js

--- commands/nsfw/Pussy.js
+++ commands/nsfw/Pussy.js
@@ -5,11 +5,15 @@ module.exports = {
  name: "pussy",
  description: "drop Pussy pictures",
  run: async (client, message, args) => {
+   if (message.channel.nsfw) {
 	const image = await nsfw.pussy();
 	const embed = new Discord.MessageEmbed()
    		 .setTitle(`Pussy Image`)
     		.setColor("GREEN")
     		.setImage(image);
 			message.channel.send(embed);
+   } else {
+	   message.channel.send("only in NSFW channel");
+   }
   }
 }
