1893f42a09a5365778e8aefda0e0dfd77a3f4dbf
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 19:53:21 +0200

    Update image.js

--- commands/help/image.js
+++ commands/help/image.js
@@ -4,9 +4,10 @@ module.exports = {
 	name: "image",
 	description: "see help image command list",
 	usage: "image"
-}
+	run: async (client, message, args) => {
 
-run: async (client, message, args) => {
+
+
 
  const embed = new Discord.MessageEmbed()
    .setTitle("all command for image")
@@ -17,5 +18,6 @@ run: async (client, message, args) => {
    .setFooter("discord.gg/PGww3kE4eN")
      message.channel.send(embed)
   }
+}
 
 		
