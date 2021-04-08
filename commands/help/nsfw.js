5694f00c54208402d8f6ca6aea22a3d68e0c1c2c
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 20:49:36 +0200

    Update nsfw.js

--- commands/help/nsfw.js
+++ commands/help/nsfw.js
@@ -41,7 +41,7 @@ module.exports = {
 			 
 
 					 .setTimestamp()
-					 .setFooter("work in progress")    
+					 .setFooter("discord.gg/PGww3kE4eN")    
 						message.channel.send(embed);
 					}
 				};
