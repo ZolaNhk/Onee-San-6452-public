d30f84910818dca7f88962268a81b55b3baad778
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 18:50:12 +0200

    Add files via upload

---
+++ commands/help/help.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+
+
+
+module.exports = {
+        name: "help",
+        usage: "help",
+		description: "show all command",
+	      run:  async (client, message, prefix, ) => {
+	 		 const embed = new Discord.MessageEmbed()
+			   		 .setTitle(`all command :`)
+			    	 .setColor("BLACK")
+			    	 
+			    	 .addField("//nsfw",
+			    	  "see nsfw command", 
+			    	  )
+			    	 .addField("//image",
+			    	  "see command for image",
+			    	  )
+			    	  
+
+			    	  
+
+			    	 .setTimestamp()
+			    	 .setFooter("work in progress")
+				 
+			    		
+								message.channel.send(embed);
+					}
+				};
