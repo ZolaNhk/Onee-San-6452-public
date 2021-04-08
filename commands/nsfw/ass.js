75f2ed78d9d49f3eef8e4d15c0635a3ea73c7890
catnodjah <61189739+catnodjah@users.noreply.github.com>
2021-04-08 17:16:43 +0200

    Update ass.js

--- commands/nsfw/ass.js
+++ commands/nsfw/ass.js
@@ -5,11 +5,15 @@ module.exports = {
  name: "ass",
  description: "drop hentai pictures",
  run: async (client, message, args) => {
+    if (message.channel.nsfw) {
 	const image = await nsfw.ass();
 	const embed = new Discord.MessageEmbed()
    		 .setTitle(`...`)
     		.setColor("GREEN")
     		.setImage(image);
 			message.channel.send(embed);
+    } else {
+	    message.channel.send("only in NSFW channel");
+    }
   }
 }
